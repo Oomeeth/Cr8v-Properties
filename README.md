68a4e6b2a26e3a5dc266fbc838310e72edfc47bb
Cr8v Dev <cr8vdev@gmail.com>
2022-05-23 15:58:56 +0200

    Sign up completed

--- README.md
+++ README.md
@@ -3,13 +3,23 @@
 
 20/05/2022 - Getting familiar with Firebase.
 
-20/05/2022 - 21/05/2022 - Setting up Firebase and implementing user sign in with Firebase.
+20/05/2022 - 21/05/2022 - Setting up Firebase. Developing login screen. Developing sign in screen.
 
-22/05/2022 - 22/05/2022 - Refactored and cleaned up code. Implement user authentication with Firebase.
+22/05/2022 - 22/05/2022 - Implementing sign up functionality.
 
-23/05/2022 - 24/05/2022 - Code home page, add a property and delete a property.
+23/05/2022 - Sign ip functionality completed. Refactoring code. Implementing user authentication.
 
-25/05/2022 - Allow user to edit profile.
+# Completed tasks:
+- Learning Android + Java.
+- Learn Firebase.
+- Setting up Firebase.
+- Design sign up screen + develop functionality for sign up screen.
+- Design log in screen
+
+# Outstanding tasks:
+- Develop functionality for log in screen.
+- Code home page, add a property, update a property entry and delete a property entry.
+- Allow user to edit profile.
 
 # Login Screen:
 ![Login](https://i.ibb.co/Qk7xLYr/1.png)
--- app/src/main/java/com/example/gettingstarted/SignUp.java
+++ app/src/main/java/com/example/gettingstarted/SignUp.java
@@ -7,22 +7,34 @@ import android.os.Bundle;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
 import android.widget.EditText;
 import android.content.Context;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ProgressBar;
 import android.view.View;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import androidx.annotation.NonNull;
 import android.view.WindowManager;
 import android.text.TextUtils;
 import android.graphics.drawable.Drawable;
+import java.util.Map;
+import java.util.HashMap;
 
 public class SignUp extends AppCompatActivity {
 
+    public static final String SIGN_UP_MESSAGE = "com.example.gettingstarted.MESSAGE";
+
     private FirebaseAuth mAuth;
+
+    private FirebaseFirestore db;
 
     private Context mContext;
 
@@ -76,6 +88,7 @@ public class SignUp extends AppCompatActivity {
         loadingProgressBar = (ProgressBar)findViewById(R.id.loading_icon_sign_up);
     }
 
+    //Actions to take if user enters invalid data.
     private void ErrorAction(EditText _editTextInput, TextView _errorTextView, Drawable _backgroundForEditText, String errorMessage)
     {
         _editTextInput.setBackgroundDrawable(_backgroundForEditText);
@@ -83,6 +96,7 @@ public class SignUp extends AppCompatActivity {
         _errorTextView.setVisibility(View.VISIBLE);
     }
 
+    //Actions to take if user enters correct data.
     private void CorrectAction(EditText _editTextInput, TextView _errorTextView, Drawable _backgroundForEditText)
     {
         _editTextInput.setBackgroundDrawable(_backgroundForEditText);
@@ -90,6 +104,7 @@ public class SignUp extends AppCompatActivity {
         _errorTextView.setVisibility(View.INVISIBLE);
     }
 
+    //Ensures fields are not empty.
     private boolean Validation_IsInputEmpty(EditText _editTextInput, TextView _errorTextView, Drawable _correctBackgroundForEditText, Drawable _errorBackgroundForEditText)
     {
         String errorMessage = "Field cannot be empty!";
@@ -108,6 +123,7 @@ public class SignUp extends AppCompatActivity {
         }
     }
 
+    //Ensures passwords match.
     private boolean Validation_ArePasswordsDifferent(EditText _editTextPassword, TextView _textViewError, EditText _editTextPasswordConfirm, TextView _textViewErrorConfirm, Drawable _correctBackgroundForEditText, Drawable _errorBackgroundForEditText)
     {
         String errorMessage = "Passwords do not match!";
@@ -135,6 +151,7 @@ public class SignUp extends AppCompatActivity {
         }
     }
 
+    //Ensures passwords are more than 6 characters (Firebase Auth requirement)
     private boolean Validation_ArePasswordsSixCharactersOrMore(EditText _editTextPassword, TextView _textViewError, EditText _editTextPasswordConfirm, TextView _textViewErrorConfirm, Drawable _correctBackgroundForEditText, Drawable _errorBackgroundForEditText)
     {
         boolean hasErrors = false;
@@ -161,7 +178,8 @@ public class SignUp extends AppCompatActivity {
         return hasErrors;
     }
 
-    private void SubmitUserDataToFireBase(EditText _editTextEmail, EditText _editTextPassword)
+    //Submits data (email and password) to Firebase Auth. Additional arguments are sent to SubmitUserData().
+    private void SubmitAuthData(EditText _editTextEmail, EditText _editTextPassword, String _username, String _name, String _surname)
     {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
 
@@ -173,10 +191,7 @@ public class SignUp extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task){
                         if(task.isSuccessful()){
-                            Intent intent = new Intent(mContext, MainActivity.class);
-                            String message = "Successfully Signed Up";
-                            intent.putExtra(SIGN_UP_MESSAGE, message);
-                            startActivity(intent);
+                            SubmitUserData(_username, _name, _surname, _editTextEmail.getText().toString(), task.getResult().getUser().getUid());
                         }
                         else{
                             loadingBackgroundView.setVisibility(View.INVISIBLE);
@@ -185,6 +200,45 @@ public class SignUp extends AppCompatActivity {
                         }
                     }
                 });
+
+
+    }
+
+    //Submits data (name, surname, username, email, UID) to Firebase FireStore. Stored as nested fields (UID -> username, name, surname, email, uid).
+    private void SubmitUserData(String _username, String _name, String _surname, String _email, String _userID)
+    {
+        Map<String, Object> userRoot = new HashMap<>();
+        Map<String, Object> user = new HashMap<>();
+
+        user.put("username", _username);
+        user.put("name", _name);
+        user.put("surname", _surname);
+        user.put("email", _email);
+        user.put("uid", _userID);
+
+        userRoot.put(_userID, user);
+
+        db.collection("users").document("user_details").update(userRoot).addOnSuccessListener(
+                new OnSuccessListener<Void>(){
+                    @Override
+                    public void onSuccess(Void aVoid)
+                    {
+                        Intent intent = new Intent(mContext, MainActivity.class);
+                        String message = "Successfully Signed Up";
+                        intent.putExtra(SIGN_UP_MESSAGE, message);
+                        startActivity(intent);
+                    }
+                }
+        ).addOnFailureListener(
+                new OnFailureListener(){
+                    @Override
+                    public void onFailure(@NonNull Exception e){
+                        loadingBackgroundView.setVisibility(View.INVISIBLE);
+                        loadingProgressBar.setVisibility(View.INVISIBLE);
+                        Toast.makeText(SignUp.this, "Sign Up Failed!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
     }
 
     @Override
@@ -193,6 +247,8 @@ public class SignUp extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         mAuth = FirebaseAuth.getInstance();
+
+        db = FirebaseFirestore.getInstance();
 
         mContext = this;
 
@@ -247,7 +303,7 @@ public class SignUp extends AppCompatActivity {
 
         if(!hasError)
         {
-            SubmitUserDataToFireBase(emailEditText, passwordEditText);
+            SubmitAuthData(emailEditText, passwordEditText, usernameEditText.getText().toString(), nameEditText.getText().toString(), surnameEditText.getText().toString());
         }
     }
 }
